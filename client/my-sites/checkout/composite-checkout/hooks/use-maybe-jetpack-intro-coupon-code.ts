1cadb8b6282d58f3b8ba1bd8a6482a4aac4b6554
Matt Gawarecki <matt.gawarecki@gmail.com>
2021-12-08 14:41:43 -0600

    Fix incorrectly-flipped equality test

--- client/my-sites/checkout/composite-checkout/hooks/use-maybe-jetpack-intro-coupon-code.ts
+++ client/my-sites/checkout/composite-checkout/hooks/use-maybe-jetpack-intro-coupon-code.ts
@@ -5,7 +5,7 @@ import type { RequestCartProduct } from '@automattic/shopping-cart';
 const JETPACK_SEARCH_INTRO_COUPON_CODE = 'FIRSTYEAR50';
 
 const isYearly = ( product: RequestCartProduct ) => ! product.product_slug.endsWith( '_monthly' );
-const isNewPurchase = ( product: RequestCartProduct ) => product.extra.purchaseType === 'renewal';
+const isNewPurchase = ( product: RequestCartProduct ) => product.extra.purchaseType !== 'renewal';
 
 // **NOTE**: This hook can be safely deleted when we no longer need to
 // rely on auto-applied coupons for introductory new purchase pricing.
